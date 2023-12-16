e7f1398e821c8b58ae6dae865542654a383ea090
theresita28 <theresia.deubert@web.de>
2023-12-16 11:47:57 +0100

    commit

---
+++ Python/motivation.py
@@ -0,0 +1,13 @@
+# Motivation programm 
+# With the Help/ Idea of the source https://www.python-lernen.de/uebung-lobesreden.htm
+
+import random
+
+adjektivlist = [beste, liebenswürdigste, schönste, größte]
+nomenlist = [Mensch, Programmierer, Freund] 
+
+
+adjektiv = print(random.choice(adjektiv))
+nomen = print(random.choice(nomenlist))
+
+print("Du bist der " + adjektiv + " " + nomen)
