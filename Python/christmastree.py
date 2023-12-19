12159008053c0ef7f0d89e6eb4daba766d76720a
theresita28 <theresia.deubert@web.de>
2023-12-19 15:02:49 +0100

    Christmastree

---
+++ Python/christmastree.py
@@ -0,0 +1,19 @@
+#Project: Christmastree
+
+for i in range(1, 10, 1):  
+   
+   for k in range (10-i, 0, -1):
+      print(" ",end='')
+    
+   for j in range (2*i-1):
+      print("*", end='')  
+   print() 
+
+  
+   
+for k in range (9, 0, -1):
+    print(" ",end='')
+    
+print("X", end= '')
+ 
+print() 
