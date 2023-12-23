bf7bcc80de24863c7f05ba53568a1b0546d15514
theresita28 <theresia.deubert@web.de>
2023-12-23 10:56:49 +0100

    UpdateMotivation

--- Python/motivation.py
+++ Python/motivation.py
@@ -1,13 +1,28 @@
 # Motivation programm 
 # With the Help/ Idea of the source https://www.python-lernen.de/uebung-lobesreden.htm
+# Update: gendern mit chat api/ spacy
+# In Blog ausführlicher erklären über gender probleme und mit den Adjektiven...
 
 import random
 
-adjektivlist = ["beste", "liebenswürdigste", "schönste", "größte"]
-nomenlist = ["Mensch", "Programmierer", "Freund"] 
+gender = input("Bitte Geschlecht angeben(männlich, weiblich)")
 
+adjektivlist = ["beste", "liebenswürdigste", "schönste", "größte"]
+nomenlist = ["Mensch", "Programmierer", "Freund"]
 
 adjektiv = random.choice(adjektivlist)
 nomen = random.choice(nomenlist)
 
-print("Du bist der " + adjektiv + " " + nomen)
+if gender == "männlich":
+
+    print("Du bist der " + adjektiv + " " + nomen)
+elif gender == "weiblich":
+    if (nomen == "Programmierer"):
+        nomen = "Programmierierin"
+    
+    if (nomen == "Freund"):
+        nomen = "Freundin"
+
+    print("Du bist die " + adjektiv + " " + nomen)
+else:
+    print("Falsche Eingabe: Bitte männlich oder weiblich eingeben")
