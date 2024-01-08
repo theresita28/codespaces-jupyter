835d13933041f2de858603bdc524a78375f1ed4d
theresita28 <theresia.deubert@web.de>
2024-01-08 16:01:33 +0100

    Conditions

---
+++ Unterricht/conditions.py
@@ -0,0 +1,18 @@
+#Short Hand If... else
+
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+
+
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+# The pass statement
+a = 33
+b = 200
+
+if b > a:
+  pass
