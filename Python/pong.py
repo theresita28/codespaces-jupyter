232a887dfcbe5e83e82c1ead1313d6ef8636a3d0
theresita28 <theresia.deubert@web.de>
2024-01-28 14:29:09 +0100

    New game

---
+++ Python/pong.py
@@ -0,0 +1,47 @@
+# Importieren der Pygame-Bibliothek
+import pygame
+
+# initialisieren von pygame
+pygame.init()
+
+# genutzte Farbe
+ORANGE  = ( 255, 140, 0)
+ROT     = ( 255, 0, 0)
+GRUEN   = ( 0, 255, 0)
+SCHWARZ = ( 0, 0, 0)
+WEISS   = ( 255, 255, 255)
+
+# Fenster öffnen
+screen = pygame.display.set_mode((640, 480))
+
+# Titel für Fensterkopf
+pygame.display.set_caption("Unser erstes Pygame-Spiel")
+
+# solange die Variable True ist, soll das Spiel laufen
+spielaktiv = True
+
+# Bildschirm Aktualisierungen einstellen
+clock = pygame.time.Clock()
+
+# Schleife Hauptprogramm
+while spielaktiv:
+    # Überprüfen, ob Nutzer eine Aktion durchgeführt hat
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            spielaktiv = False
+            print("Spieler hat Quit-Button angeklickt")
+
+    # Spiellogik hier integrieren
+
+    # Spielfeld löschen
+    screen.fill(SCHWARZ)
+
+    # Spielfeld/figuren zeichnen
+
+    # Fenster aktualisieren
+    pygame.display.flip()
+
+    # Refresh-Zeiten festlegen
+    clock.tick(60)
+
+pygame.quit()
